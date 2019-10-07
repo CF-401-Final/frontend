9d3870cb2b463c11cc609fd3860fcfa080bc77f0
twardcox <t.ward.cox@gmail.com>
2019-10-07 14:41:49 -0700

    mob connection made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Instructor from './components/instructor.js';
 import Student from './components/student.js';
 
+
 // import './App.css';
 import Sockette from 'sockette';
 
-const socket = new Sockette('wss://ux9oyskfdg.execute-api.us-east-1.amazonaws.com/dev', {
-  timeout: 5e3,
-  maxAttempts: 10,
-  onopen: e => console.log('Connected!', e),
-  onmessage: e => console.log('Recieved', e),
-  onreconnect: e => console.log('Reconnecting...', e),
-  onmaximum: e => console.log('Stop Attempting!', e),
-  onclose: e => console.log('Closed!', e),
-  onerror: e => console.log('Error:', e)
-});
+let socket = null;
 
 function App() {
+
+  const [data, setData] = useState({
+    zeroCount:0,
+    oneCount:0,
+    twoCount:0,
+    threeCount:0,
+    fourCount: 0,
+    fiveCount:0,
+    totalConnections:0
+  });
+useEffect(
+  () => {
+    if(socket===null){
+      socket = new Sockette('wss://ux9oyskfdg.execute-api.us-east-1.amazonaws.com/dev',{
+        timeout: 5e3,
+        maxAttempts: 10,
+        onopen: e => console.log('Connected!', e),
+        onmessage: e => getData(e),
+        onreconnect: e => console.log('Reconnecting...', e),
+        onmaximum: e => console.log('Stop Attempting!', e),
+        onclose: e => console.log('Closed!', e),
+        onerror: e => console.log('Error:', e)
+      });
+    }
+    
+  },
+  [data]
+);
+function getData(e){
+  console.log(JSON.parse(e.data))
+  setData(JSON.parse(e.data));
+  console.log(data)
+}
+
+function sendData(vote){
+  socket.json(vote)
+}
+
+
   return (
     <Router>
       <div>
@@ -39,10 +70,10 @@ function App() {
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/student">
-            <Student />
+            <Student data={data} sendData={sendData}/>
           </Route>
           <Route path="/instructor">
-            <Instructor />
+            <Instructor data={data} sendData={sendData}/>
           </Route>
 
         </Switch>
